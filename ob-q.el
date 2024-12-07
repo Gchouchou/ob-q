e8d778c78aa5bc1fb35201c0c2dd9a62352f9b24
Justin <jusytinyu@gmail.com>
2024-12-06 22:04:48 -0500

    Bug fix but also more broken

--- ob-q.el
+++ ob-q.el
@@ -221,7 +221,7 @@ Return the initialized session."
   (unless (string= session "none")
     (let* ((session (or session "*org-babel-q*"))
            (buffer (get-buffer-create session)))
-      (if (not (comint-check-proc buffer))
+      (if (not (get-buffer-process buffer))
           (if (require 'q-mode "q-mode" t)                         ; check if q-mode is loaded
               (let* ((process (q q-host q-user (q-default-args)))  ; start q with defaults
                      (buffer2 (process-buffer process)))
